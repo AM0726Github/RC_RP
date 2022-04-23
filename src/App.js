cc49d93467bf3185d990191dbaf52de30a4b52fd
AM0726Github <Margaryanalik7@gmail.com>
2022-04-23 13:19:40 -0700

    App.js Import components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from "./components/Header";
+import Navigation from './components/Nav';
+import Footer from './components/Footer';
+import About from './components/About'
+import Contact from './components/Contact';
+import ProjectList from './components/Projects';
+import Resume from './components/Resume';
+import Home from './components/Home';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header>
+        <Navigation/>
+      </Header>
+      <Routes>
+        {/* <Route exact path="/" render={() => <Navigate to="/about" />} /> */}
+        <Route path="/" exact element={<Home />} />
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/projects" element={<ProjectList />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/resume" element={<Resume />}/>
+        <Route path="/home" element={<Home />}/>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
