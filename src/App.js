3aa61303712b4c1beba072d6eb39eb0944b47c89
AM0726Github <Margaryanalik7@gmail.com>
2022-04-23 13:29:51 -0700

    components helpers f/s

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Header from "./components/Header";
-import Navigation from './components/Nav';
+import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import About from './components/About'
 import Contact from './components/Contact';
